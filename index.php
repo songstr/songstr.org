5438d7404bc86abd8f7291a754b2d57b1942a35e
Melvin Carvalho <melvincarvalho@gmail.com>
2023-10-28 20:23:20 +0200

    fix php

--- index.php
+++ index.php
@@ -106,17 +106,42 @@
     <?php
       $uri = $_GET['uri'] ?? null;
       $path = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+      error_log(print_r($path, true));
       $id = end($path);
       $dataURL = $uri ?? (strlen($id) === 36 ? "https://nosdav.net/melvin/songstr/$id.json" : null);
       $songData = null;
+      error_log($dataURL);
 
       if ($dataURL) {
         $json = file_get_contents($dataURL);
+        error_log($json);
         if ($json) {
           $songData = json_decode($json, true);
         }
       }
+?>
+
+<?php
+
+if ($songData):
+    $songTitle = $songData['title'] ?? null;
+    $songDescription = $songData['creator'] ?? null;
+    $songThumbnail = $songData['image'] ?? null;
+    $youtubeURL = $songData['url']['YouTube'] ?? null;
     ?>
+
+    <!-- Open Graph Basic Info -->
+
+    <!-- Open Graph Video Info -->
+    <?php if ($youtubeURL): ?>
+        <meta property="og:video:url" content="<?php echo $youtubeURL; ?>">
+        <meta property="og:video:type" content="text/html">
+        <meta property="og:video:width" content="1280"> <!-- Adjust width based on your video's dimensions -->
+        <meta property="og:video:height" content="720"> <!-- Adjust height based on your video's dimensions -->
+    <?php endif; ?>
+
+<?php endif; ?>
+
 
     <script type="application/ld+json" id="songData">
       <?= json_encode($songData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) ?>
@@ -130,7 +155,7 @@
       <meta property="og:type" content="music.song" />
       <meta property="og:url" content="<?= "https://songstr.org/$id" ?>" />
       <meta property="og:image" content="<?= htmlspecialchars($songData['image'] ?? 'default-image-url') ?>" />
-      <meta property="og:description" content="Listen to <?= htmlspecialchars($songData['title'] ?? 'Title') ?> by <?= htmlspecialchars($songData['creator']['title'] ?? 'Creator') ?>" />
+      <meta property="og:description" content="Listen to <?= htmlspecialchars($songData['title'] ?? 'Title') ?> by <?= htmlspecialchars($songData['creator'] ?? 'Creator') ?>" />
       <meta property="music:musician" content="<?= htmlspecialchars($songData['creator'] ?? 'Creator') ?>" />
 
     <?php endif; ?>
